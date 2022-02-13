c43c61c2aa88b92844246e429006980ea13015a0
Terry Fleury <terrencegf@gmail.com>
2022-02-12 21:22:16 -0600

    Allow 'update' argument when building

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Optional command line argument 'update'
+if [[ -n $1 && $1 == 'update' ]] ; then
+    pio update
+fi
+
 DATE=`date +%F_%H-%M`
 platformio run -e rambo
 scp -o port=9022 .pio/build/rambo/firmware.hex \
