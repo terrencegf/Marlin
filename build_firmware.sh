6f08d37b8ffc747073d9c4774b82859484b99520
Terry Fleury <terrencegf@gmail.com>
2022-08-04 21:38:59 -0500

    New PC yak

--- build_firmware.sh
+++ build_firmware.sh
@@ -8,6 +8,6 @@ fi
 
 DATE=`date +%F_%H-%M`
 platformio run -e rambo
-scp -o port=9022 .pio/build/rambo/firmware.hex \
-    tfleury@192.168.0.3:/cygdrive/c/xfer/firmware-${DATE}.hex
+scp -o port=9222 .pio/build/rambo/firmware.hex \
+    tfleury@192.168.0.2:/cygdrive/c/xfer/firmware-${DATE}.hex
 pio system prune -f
