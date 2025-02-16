aec9573b84b7d5c473cf42db130147b826f330dd
Terry Fleury <terrencegf@gmail.com>
2025-02-16 17:08:15 -0600

    Fix SSH port for mud.fleury.tv

--- build_firmware.sh
+++ build_firmware.sh
@@ -8,6 +8,6 @@ fi
 
 DATE=`date +%F_%H-%M`
 platformio run -e rambo
-scp -o port=9222 .pio/build/rambo/firmware.hex \
-    tfleury@mud.fleury.tv:/cygdrive/c/xfer/firmware-${DATE}.hex
+scp .pio/build/rambo/firmware.hex \
+    tfleury@mud.fleury.tv:xfer/firmware-${DATE}.hex
 pio system prune -f
